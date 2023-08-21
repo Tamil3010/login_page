9a4cdbef9f01d45269490119ab1e733d7033b0fa
Tamil3010 <131370005+Tamil3010@users.noreply.github.com>
2023-08-21 20:38:33 +0530

    Update main.dart
    just comment line check

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+//this is my edit from another acc
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
